a6a44e0bc8be7c1f6dbeae84a567b59e4cbdcf2e
khallu35 <73116822+khallu35@users.noreply.github.com>
2020-10-20 08:19:26 +0530

    Add files via upload

---
+++ Groups.java
@@ -0,0 +1,25 @@
+public class Groups
+{
+	String name;
+	String mobno;
+public static void main(String args[])
+{
+	Groups g1 = new Groups();
+	System.out.println(g1.name="likitha");
+	System.out.println(g1.mobno="22102522");
+
+	Groups g2 = new Groups();
+	System.out.println(g1.name="mamatha");
+	System.out.println(g1.mobno="22102524");
+
+	Groups g3 = new Groups();
+	System.out.println(g1.name="bhavya");
+	System.out.println(g1.mobno="22102528");
+
+	Groups g4 = new Groups();
+	System.out.println(g1.name="mallikarjuna");
+	System.out.println(g1.mobno="22102529");
+
+
+}
+}
